cc4a2e764f91cf549c1e11586de465a846134b77
ghemanth3 <160579449+ghemanth3@users.noreply.github.com>
2024-02-21 05:28:12 +0000

    2

--- .devcontainer/hem/main.tf
+++ .devcontainer/hem/main.tf
@@ -6,3 +6,7 @@ resource "aws_instance" "hem_instance" {
     ami = "ami-0c7217cdde317cfec"
     instance_type = "t2.micro"
 }
+
+/*resource "aws_s3_bucket" "hem_s3" {
+    bucket = "hemanth-s3-bucket-123" 
+}*/
